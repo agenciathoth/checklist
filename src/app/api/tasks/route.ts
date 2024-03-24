7ad4de32d61d13687180c6e69d5a5cec8c230f23
VilsonSampaio <vilson.sampaio04@gmail.com>
2024-03-23 22:01:18 -0300

    fix: parsing due as date to save on data base

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,11 +1,10 @@
 import { nextAuthOptions } from "@/config/auth";
 import { prismaClient } from "@/lib/prisma";
-import { createCustomerSchema } from "@/validators/customer";
 import { createTaskSchema } from "@/validators/task";
+import { parseISO } from "date-fns";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import slugify from "slugify";
-import { ZodError, z } from "zod";
+import { ZodError } from "zod";
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(nextAuthOptions);
@@ -22,7 +21,7 @@ export async function POST(request: NextRequest) {
       data: {
         title,
         description,
-        due,
+        due: parseISO(due),
         responsible,
         customer: {
           connect: { id: customerId },
@@ -40,6 +39,7 @@ export async function POST(request: NextRequest) {
       return new NextResponse("Validation Error", { status: 400 });
     }
 
+    console.log(error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
