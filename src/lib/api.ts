90ac65ce0d41808991e52637a1c04931920b5c60
VilsonSampaio <vilson.sampaio04@gmail.com>
2024-03-23 21:07:49 -0300

    feat: update api url according node env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { env } from "@/config/env";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: (env.NODE_ENV === "production"
+  baseURL: (process.env.NODE_ENV === "production"
     ? "https://checklist-next.vercel.app/"
     : "http://localhost:3000/"
   ).concat("api/"),
